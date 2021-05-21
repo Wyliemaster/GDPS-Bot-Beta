470331dd616bf0e65eae6afc4739c4db58c542fb
Wyliemaster <62946885+Wyliemaster@users.noreply.github.com>
2021-05-21 19:54:18 +0100

    big oopsie

--- commands/cron.js
+++ commands/cron.js
@@ -3,7 +3,7 @@ const botconfig = require("../botsettings.json");
 const request = require('request');
 module.exports.run = async (bot, message, args) => {
     if (!botconfig.setup.cronEnabled)
-        message.reply(`The cron command is currently disabled`);
+        return message.reply(`The cron command is currently disabled`);
     let start = Math.floor(Date.now());
     message.channel.send(`Starting the Cron Job...`);
     request.get(`http://${botconfig.setup.server}/tools/cron/cron.php`, function callback(err, httpResponse, body) {
